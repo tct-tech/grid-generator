ae622327399471a57ca35472d4653b6f63cf693a
tct-tech <lemuellina1@gmail.com>
2023-08-25 15:49:14 -0700

    edit title

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
  * @Author: Your name
  * @Date:   2023-04-26 16:42:42
  * @Last Modified by:   Your name
- * @Last Modified time: 2023-08-25 15:36:35
+ * @Last Modified time: 2023-08-25 15:48:57
  */
 import React from 'react';
 import '../styles/styles.css';
@@ -10,7 +10,7 @@ import '../styles/styles.css';
 const Header = () => {
     return(
         <div className="header">
-            <h1 className='headerText'>TCT Grid Pattern Generator</h1>
+            <h1 className='headerText'>Grid Pattern Generator</h1>
         </div>
     );
 };
