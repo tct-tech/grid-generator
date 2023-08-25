e9ed2aaa1787f085451359f1da583c08d4331464
tct-tech <lemuellina1@gmail.com>
2023-08-25 15:42:24 -0700

    adjust title size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
+/**
+ * @Author: Your name
+ * @Date:   2023-04-26 16:42:42
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2023-08-25 15:36:35
+ */
 import React from 'react';
 import '../styles/styles.css';
 
 const Header = () => {
     return(
-        <div className="header">TCT Grid Pattern Generator</div>
+        <div className="header">
+            <h1 className='headerText'>TCT Grid Pattern Generator</h1>
+        </div>
     );
 };
 
